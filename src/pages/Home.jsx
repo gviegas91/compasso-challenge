5a087179c0eb70b3a08f83a00f84287ba760c821
gviegas91 <gviegas91@gmail.com>
2021-04-09 19:20:14 -0300

    Add initial project config
    - Eslint airbnb
    - Prettier
    - Editorconfig
    - Folder structure
    - Remove unused files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  const name = 'Capt. Janeway';
+  return (
+    <div className="a">
+      <div className="a"></div>
+    </div>
+  );
+};
+
+export default HomePage;
